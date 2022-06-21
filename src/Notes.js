46641188967f7d70c41bd85ed5c324b6365fdb5a
Sylwia Keżel <sylwia@mk-home.pl>
2022-06-21 11:25:56 +0200

    add new file

---
+++ src/Notes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Notes() {
+    const header = <h3>Lista notatek</h3>
+    return (
+        <div>
+            {header}
+        </div>
+    );
+}
+export default Notes;
